99b31771d25cfd8de90cac89ab636819f08dfc67
madhushani-29 <madhushani2904@gmail.com>
2024-06-02 09:41:50 +0530

    home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,26 @@
 import landing from "../assets/landing.png";
 import appDownload from "../assets/appDownload.png";
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
 
     return (
         <div className='flex flex-col gap-12'>
             <div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
-                <h1 className="text-5xl font-bold tracking-tight text-orange-600">Tuck into a takeaway today</h1>
-                <span className="text-xl">Food is just a click away!</span>
-                Search Bar
+                <h1 className="text-5xl font-bold tracking-tight text-teal-700">Wanderlust Chronicles: Global Adventure Stories</h1>
+                <span className="text-xl px-10">Dive into Wanderlust Diaries for captivating global adventure stories, travel tips, and destination guides. Join us as we explore unique cultures, breathtaking landscapes, and unforgettable journeys around the world. Your adventure begins here!</span>
+                <Button className="bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded md:mx-80 mx-20  mb-10 h-14">Join Us</Button>
             </div>
             <div className="grid md:grid-cols-2 gap-5">
-                <img src={landing} alt="mobile preview image" />
-                <div className="flex flex-col items-center justify-center gap-4 text-center">
+                <img src={landing} alt="mobile preview image" className="ml-10"/>
+                <div className="flex flex-col items-center justify-center gap-4 text-center ">
                     <span className="font-bold text-3xl tracking-tighter">
-                        Order takeaway even faster!
+                        Your Mobile Travel Companion!
                     </span>
                     <span>
-                        Download the MernEats App for fast ordering and personalized recommendations
+                        Get the WanderEase App for seamless travel planning and personalized recommendations on the go.
                     </span>
-                    <img src={appDownload} alt="Playstore apple store" />
+                    <img src={appDownload} alt="Playstore apple store"  />
                 </div>
             </div>
         </div>
